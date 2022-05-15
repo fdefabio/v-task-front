fc9f432f01a29cd5a94109ef8d66a649388508de
fabio <fabioannypro47@gmail.com>
2022-05-14 21:07:13 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { IndexComponent } from './index/index.component';
+import { HistoryComponent } from './history/history.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    IndexComponent,
+    HistoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
